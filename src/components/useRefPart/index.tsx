4d11b163d9ccbdfadbb805e31f8dbca96540e548
桑榆 <2249736997@qq.com>
2025-10-07 23:58:09 +0800

    feat:useRef的hooks

--- src/components/useRefPart/index.tsx
+++ src/components/useRefPart/index.tsx
@@ -1,14 +1,28 @@
-import { useRef } from 'react';
+import {useEffect, useRef} from 'react';
 
 const UseRefPart = () =>{
     const inputRef = useRef<HTMLInputElement>(null);
-
+    const timeRef = useRef<NodeJS.Timeout|null>(null);
     const focus = () => inputRef.current?.focus();
+    useEffect(() => {
+        timeRef.current = setInterval(()=>console.log('tick'),100 )
+        return () => {
+            if (timeRef.current) clearInterval(timeRef.current);
+        }
+    }, []);
 
+    const stop = () =>{
+        if (timeRef.current){
+            clearInterval(timeRef.current);
+            timeRef.current = null;
+        }
+    }
     return (
         <>
             <input ref={inputRef} placeholder="我会被聚焦" />
             <button onClick={focus}>聚焦</button>
+
+            <button onClick={stop}>停止计时器</button>
         </>
     );
 }
