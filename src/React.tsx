0924a5e81407bb8c0916658edc7194d800b3bfa8
桑榆 <2249736997@qq.com>
2025-10-14 11:04:17 +0800

    feat:路由的初始化

---
+++ src/React.tsx
@@ -0,0 +1,9 @@
+const React = () => {
+    return (
+        <>
+            <h2>React</h2>
+        </>
+    );
+};
+
+export default React;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
-import {createRoot} from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import './components/Message/index.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import React from './React.tsx';
+import './components/Message/index.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-    <App/>
-)
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='/react' element={<React />} />
+        </Routes>
+    </BrowserRouter>,
+);
